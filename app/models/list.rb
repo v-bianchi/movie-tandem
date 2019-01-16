48f6d86efe77d471327019048055d03e91e0cada
Victor Bianchi <vbianchi90@gmail.com>
2019-01-15 23:56:55 -0200

    add has_many movies to list

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,7 @@
 class List < ApplicationRecord
   belongs_to :user_1, class_name: 'User'
   belongs_to :user_2, class_name: 'User'
+  has_many :movies
 
   validates :user_2_id, :uniqueness: { scope: :user_1_id, message: "You already have a watchlist with this user!" }
 
