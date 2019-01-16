00e7b256abf799f133e2dd1414dea17d16df3b4a
Victor Bianchi <vbianchi90@gmail.com>
2019-01-16 00:10:23 -0200

    fix realine issue and fix typo on user model

--- app/models/request.rb
+++ app/models/request.rb
@@ -2,5 +2,5 @@ class Request < ApplicationRecord
   belongs_to :sender, class_name: 'User'
   belongs_to :receiver, class_name: 'User'
 
-  validates :receiver_id, :uniqueness: { scope: :sender_id, message: "You already sent an invitation to this user!" }
+  validates :receiver_id, uniqueness: { scope: :sender_id, message: "You already sent an invitation to this user!" }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def requests_sent
+    Request.where(sender: self)
+  end
+
+  def requests_received
+    Request.where(receiver: self)
+  end
 end
