62359a3073f43128ff1d7bf7abb159634e2afa5c
Victor Bianchi <vbianchi90@gmail.com>
2019-01-22 15:14:39 -0200

    try to setup dotenv

--- app/javascript/main_react/containers/add_movie_form.jsx
+++ app/javascript/main_react/containers/add_movie_form.jsx
@@ -3,9 +3,6 @@ import { connect } from "react-redux"
 
 import SuggestionsBox from './suggestions_box'
 
-import TMDB_API_KEY from '../secret.js' //TODO: FIND BETTER SOLUTION (ENV VARIABLE?)
-
-
 class AddMovieForm extends Component {
   constructor(props) {
     super(props);
@@ -30,7 +27,7 @@ class AddMovieForm extends Component {
     this.setState({ userInput : event.currentTarget.value})
     setTimeout(() => {
       if(this.state.userInput.length > 3) {
-        const url = `https://api.themoviedb.org/3/search/movie?api_key=${TMDB_API_KEY}&query=${this.state.userInput}&page=1&include_adult=false`
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.TMDB_API_KEY}&query=${this.state.userInput}&page=1&include_adult=false`
         fetch(url)
           .then(response => response.json())
           .then((data) => {
