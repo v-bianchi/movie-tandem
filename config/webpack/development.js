bc6189e8fe43c082189dd03c7521cfe3cc230a05
Victor Bianchi <vbianchi90@gmail.com>
2019-02-26 12:03:55 -0300

    config dotenv

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -2,4 +2,9 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
 
+environment.plugins.prepend(
+  'Dotenv',
+  new Dotenv()
+)
+
 module.exports = environment.toWebpackConfig()
